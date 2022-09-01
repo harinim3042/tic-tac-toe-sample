8bd849347ae5abc6ca48ff8ec892c37503001723
harinim3042 <harinim3042@gmail.com>
2022-09-01 10:48:54 +0530

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React from 'react';
 import './styles.css';
-
-import { Line } from 'react-chartjs-2';
-
-const data = {
-  labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-  datasets: [
-    {
-      label: 'First dataset',
-      data: [33, 53, 85, 41, 44, 65],
-      fill: true,
-      backgroundColor: 'rgba(75,192,192,0.2)',
-      borderColor: 'rgba(75,192,192,1)',
-    },
-    {
-      label: 'Second dataset',
-      data: [33, 25, 35, 51, 54, 76],
-      fill: false,
-      borderColor: '#742774',
-    },
-  ],
-};
+import DoughnutChart from './Donught.js';
+import LineChart from './Linechart.js';
 
 export default function App() {
   return (
     <div className="App">
-      <Line data={data} />
+      <div className="container">
+        <DoughnutChart />
+        <LineChart />
+      </div>
     </div>
   );
 }
---
+++ src/Linechart.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Line } from 'react-chartjs-2';
+function LineChart() {
+  const data = {
+    labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+    datasets: [
+      {
+        label: 'First dataset',
+        data: [33, 53, 85, 41, 44, 65],
+        fill: true,
+        backgroundColor: 'rgba(75,192,192,0.2)',
+        borderColor: 'rgba(75,192,192,1)',
+      },
+      {
+        label: 'Second dataset',
+        data: [33, 25, 35, 51, 54, 76],
+        fill: false,
+        borderColor: '#742774',
+      },
+    ],
+  };
+  const options = {
+    plugins: {
+      title: {
+        display: true,
+        text: 'Line Chart',
+        color: 'blue',
+        font: {
+          size: 34,
+        },
+        padding: {
+          top: 30,
+          bottom: 30,
+        },
+        responsive: true,
+        animation: {
+          animateScale: true,
+          color: true,
+        },
+      },
+    },
+  };
+  return (
+    <div>
+      <Line data={data} options={options} />
+    </div>
+  );
+}
+
+export default LineChart;
